1bc0f3346a59dd94c8747cdc3d78036eb2d3b889
krishnamsjyothis-stack <krishnamsjyothis@gmail.com>
2026-09-08 15:34:13 +0530

    Delete backend directory

--- backend/test-env.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-console.log("EMAIL_USER exists:", !!process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-console.log("JWT_SECRET exists:", !!process.env.JWT_SECRET);
